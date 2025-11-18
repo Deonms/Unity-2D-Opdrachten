c691f5b69101eb29a03fd364940ca90180c6e15b
DeonsGaming <Deon.mob@gmail.com>
2025-11-18 11:53:52 +0100

    edit code

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    // [SerializeField] private float _X = 5;
+    [SerializeField] private float _Xspeed = 0.05f;
+    [SerializeField] private float _Yspeed = 0.05f;
     void Start()
     {
 
@@ -18,25 +19,25 @@ public class PlayerInput : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             print("ik heb W ingedrukt");
-            Vector3 position = new Vector3(X, Y = (float)(0.05), 0);
+            Vector3 position = new Vector3(X, Y = (_Xspeed), 0);
             transform.position += new Vector3(X, Y, 0);
         }
         else if (Input.GetKey(KeyCode.S))
         {
             print("Ik heb S ingedrukt");
-            Vector3 position = new Vector3(X, Y = (float)(0.05), 0);
+            Vector3 position = new Vector3(X, Y = (_Xspeed), 0);
             transform.position -= new Vector3(X, Y, 0);
         }
         if (Input.GetKey(KeyCode.A))
         {
             print("Ik heb A ingedrukt");
-            Vector3 position = new Vector3(X = (float)(0.05), Y, 0);
+            Vector3 position = new Vector3(X = (_Yspeed), Y, 0);
             transform.position -= new Vector3(X, Y, 0);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             print("Ik heb D ingedrukt");
-            Vector3 position = new Vector3(X = (float)(0.05), Y, 0);
+            Vector3 position = new Vector3(X = (_Yspeed), Y, 0);
             transform.position += new Vector3(X, Y, 0);
         }
     }
