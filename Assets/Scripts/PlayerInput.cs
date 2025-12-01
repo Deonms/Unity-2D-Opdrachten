de9ec7ca2a7b05eb49b41c25cb6fde7edcece44d
DeonsGaming <Deon.mob@gmail.com>
2025-12-01 14:51:20 +0100

    Update PlayerInput.cs

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -19,25 +19,25 @@ public class PlayerInput : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             print("ik heb W ingedrukt");
-            Vector3 position = new Vector3(X, Y = (_Xspeed), 0);
+            Vector3 position = new Vector3(X, Y = (_Yspeed), 0);
             transform.position += new Vector3(X, Y, 0);
         }
         else if (Input.GetKey(KeyCode.S))
         {
             print("Ik heb S ingedrukt");
-            Vector3 position = new Vector3(X, Y = (_Xspeed), 0);
+            Vector3 position = new Vector3(X, Y = (_Yspeed), 0);
             transform.position -= new Vector3(X, Y, 0);
         }
         if (Input.GetKey(KeyCode.A))
         {
             print("Ik heb A ingedrukt");
-            Vector3 position = new Vector3(X = (_Yspeed), Y, 0);
+            Vector3 position = new Vector3(X = (_Xspeed), Y, 0);
             transform.position -= new Vector3(X, Y, 0);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             print("Ik heb D ingedrukt");
-            Vector3 position = new Vector3(X = (_Yspeed), Y, 0);
+            Vector3 position = new Vector3(X = (_Xspeed), Y, 0);
             transform.position += new Vector3(X, Y, 0);
         }
         if (Input.GetKey(KeyCode.R))
