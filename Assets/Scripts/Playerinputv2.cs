4e26b6938353da25ec47667105aa415ebd1a3209
DeonsGaming <Deon.mob@gmail.com>
2025-12-02 11:34:00 +0100

    prefebs

--- Assets/Scripts/Playerinputv2.cs
+++ Assets/Scripts/Playerinputv2.cs
@@ -1,9 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 public class Playerinputv2 : MonoBehaviour
 {
     [SerializeField] private string _cointag = "coin";
     [SerializeField] private string _Death = "deathblock";
+    [SerializeField] private int _scoreboard;
+    [SerializeField] private float _respawnY = 0.2249999f;
+    [SerializeField] private float _respawnX = 2.076958f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,7 +17,7 @@ public class Playerinputv2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -23,7 +27,7 @@ public class Playerinputv2 : MonoBehaviour
         }
         if(collision.gameObject.CompareTag(_Death))
         {
-            Vector3 position = new Vector3(2.076958f, 0.2249999f, -1f);
+            Vector3 position = new Vector3(_respawnX, _respawnY, -1f);
             transform.position = position;
         }
     }
