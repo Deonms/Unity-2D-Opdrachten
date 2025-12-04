b09ed41194f120804254422fd359420e62effb9a
DeonsGaming <Deon.mob@gmail.com>
2025-12-04 10:27:54 +0100

    Push Button Script

--- Assets/Scripts/PushBlockevents.cs
+++ Assets/Scripts/PushBlockevents.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class PushBlockevents : MonoBehaviour
 {
+    [SerializeField] private string _button = "PushBlockButton";
+    [SerializeField] private string _destroywall = "DestroyPushwall1";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(_button))
+        {
+
+        }
     }
 }
