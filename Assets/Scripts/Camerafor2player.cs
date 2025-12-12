562ad399c42063dc3f215c3a3a10857befb30871
DeonsGaming <Deon.mob@gmail.com>
2025-12-12 11:01:56 +0100

    edited lvl and removed notes

--- Assets/Scripts/Camerafor2player.cs
+++ Assets/Scripts/Camerafor2player.cs
@@ -20,34 +20,25 @@ public class Camerafor2player : MonoBehaviour
     {
 
     }
-    // Follow Two Transforms with a Fixed-Orientation Camera
     public void FixedCameraFollowSmooth(Camera _maincamera, Transform _player1, Transform _player2)
     {
-        // How many units should we keep from the players
         float zoomFactor = 1.5f;
         float followTimeDelta = 0.8f;
 
-        // Midpoint we're after
         Vector3 midpoint = (_player1.position + _player2.position) / 2f;
 
-        // Distance between objects
         float distance = (_player1.position - _player2.position).magnitude;
 
-        // Move camera a certain distance
         Vector3 cameraDestination = midpoint - _maincamera.transform.forward * distance * zoomFactor;
 
-        // Adjust ortho size if we're using one of those
         if (_maincamera.orthographic)
         {
-            // The camera's forward vector is irrelevant, only this size will matter
             _maincamera.orthographicSize = distance;
             
         }
 
-        // You specified to use MoveTowards instead of Slerp
         _maincamera.transform.position = Vector3.Slerp(_maincamera.transform.position, cameraDestination, followTimeDelta);
 
-        // Snap when close enough to prevent annoying slerp behavior
         if ((cameraDestination - _maincamera.transform.position).magnitude <= 0.05f)
             _maincamera.transform.position = cameraDestination;
     }
